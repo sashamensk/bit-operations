a94a23e32fc16c860c9ed4730f0f1ca5197c683a
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-27 10:38:13 +0000

    Replace NumbersExtension.cs

--- BitOperationsTask/NumbersExtension.cs
+++ BitOperationsTask/NumbersExtension.cs
@@ -18,11 +18,11 @@ namespace BitOperationsTask
         /// <exception cref="ArgumentOutOfRangeException">Thrown when i or j is less than 0 or more than 31.</exception>
         /// <exception cref="ArgumentException">Thrown when i is more than j.</exception>
         /// <example>
-        /// Destination number = 2728, Source number = 655, i = 3, j = 8, Result = 2680
-        /// Destination number = 554216104, Source number = 15, i = 0, j = 31, Result = 15
-        /// Destination number = -55465467, Source number = 345346, i = 0, j = 31, Result = 345346
-        /// Destination number = 554216104, Source number = 4460559, i = 11, j = 18, Result = 554203816
-        /// Destination number = -1, Source number = 0, i = 31, j = 31, Result = 2147483647
+        /// Destination number = 2728, Source number = 655, i = 3, j = 8, Result = 2680,
+        /// Destination number = 554216104, Source number = 15, i = 0, j = 31, Result = 15,
+        /// Destination number = -55465467, Source number = 345346, i = 0, j = 31, Result = 345346,
+        /// Destination number = 554216104, Source number = 4460559, i = 11, j = 18, Result = 554203816,
+        /// Destination number = -1, Source number = 0, i = 31, j = 31, Result = 2147483647.
         /// </example>
         public static int InsertNumberIntoAnother(int destinationNumber, int sourceNumber, int i, int j)
         {
